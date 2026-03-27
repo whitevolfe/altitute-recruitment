c439f9a8f69077a54b9725dd4954afaeb11a09bd
Azeem <Azeem@10XTEK.com>
2026-03-27 20:21:22 +0530

    sitemap updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+import sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+
+    sitemap({
+      hostname: 'https://www.altitudegroup.io',
+      dynamicRoutes: [
+        '/about',
+        '/about/altitude-group',
+        '/about/leadership',
+        '/global',
+        '/wellness',
+        '/packages/altitude-trinity',
+        '/packages/movement-therapy',
+        '/talent',
+        '/contact',
+        '/blogs',
+      ],
+    }),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 }));
